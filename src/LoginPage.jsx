6e2f76f4c538571a34e3fe9db2e5850880f611fe
nishidalal <nishi.dalal@impelsys.com>
2026-03-13 16:09:46 +0530

    added console for window object

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -1,10 +1,8 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import "./login.css";
 
 const LoginPage = () => {
-  const [loggedIn, setLoggedin] = useState(false);
-  const [toastData, setToastData] = useState({});
-
+  console.log("window---", window);
   const waitForSDKAndRenderForm = () => {
     const maxWaitTime = 10000; // 10 seconds
     const pollInterval = 100; // Check every 100ms
@@ -64,41 +62,6 @@ const LoginPage = () => {
     // loadAuth(window);
     waitForSDKAndRenderForm();
   }, []);
-
-  useEffect(() => {
-    if (
-      window.IIRISPassport &&
-      window.IIRISPassport.irisLoginCallback === "function" &&
-      window.IIRISPassport.irisLoginCallback.success
-    ) {
-      console.log(window.IIRISPassport.irisLoginCallback);
-      setToastData({
-        show: true,
-        message:
-          window.IIRISPassport.irisLoginCallback?.data?.message ||
-          window.IIRISPassport.irisLoginCallback?.error?.message,
-      });
-      if (window.IIRISPassport.irisLoginCallback.success == true) {
-        setLoggedin(true);
-      }
-    }
-  }, [window.IIRISPassport.irisLoginCallback]);
-
-  useEffect(() => {
-    if (
-      window.IIRISPassport &&
-      window.IIRISPassport.irisRegisterCallback === "function" &&
-      window.IIRISPassport.irisRegisterCallback.success
-    ) {
-      console.log(window.IIRISPassport.irisRegisterCallback);
-      setToastData({
-        show: true,
-        message:
-          window.IIRISPassport.irisRegisterCallback?.data?.message ||
-          window.IIRISPassport.irisRegisterCallback?.error?.message,
-      });
-    }
-  }, [window.IIRISPassport.irisRegisterCallback]);
 
   return (
     <div className="page">
